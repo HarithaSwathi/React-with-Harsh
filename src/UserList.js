1aa384d99fc760560738229f938818a5345f5fb2
HarithaSwathi <haritaswathi123@gmail.com>
2025-02-13 20:48:27 +0530

    Created UserList as Class component

--- src/UserList.js
+++ src/UserList.js
@@ -1,9 +1,19 @@
-function UserList() {
-    return ( 
-    <>
-     <div>Welcome to the UseList page</div>
-    </> 
-    );
+import React from "react"
+class UserList extends React.Component {
+    state = {  } 
+    render() { 
+        return (Hi);
+    }
 }
-
+ 
 export default UserList;
+
+// function UserList() {
+//     return ( 
+//     <>
+//      <div>Welcome to the UseList page</div>
+//     </> 
+//     );
+// }
+
+// export default UserList;
